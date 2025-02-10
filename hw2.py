5d8a3819894876fc2009ccf8cd019a2bc8a530ae
Forrest Johnson <forrestjohnson@berkeley.edu>
2025-02-09 17:52:20 -0800

    hw2.py

---
+++ hw2.py
@@ -0,0 +1,20 @@
+# 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+#cd ~/Desktop
+# 2) What does ~/ mean?
+# A shortcut for the home directory. 
+# 3) What's the difference between and abolute and a relative path in your terminal?
+# An absolute path in my termial means I navigate directly to my chosen folder and file. A relative path in my terminal means I successively open folders until I navigate to my file. 
+# 4) What command would bring you back to your home directory?
+#cd ~
+# 5) If you called rm ./ in your current working directory, what would it do? 
+#Removes all entries in all files under the directory. 
+# 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+#git add adds a repository to your terminal. git commit bypasses the warnings about adding or pushing entries to and from github. git push pushes entries from the terminal onto your online repository. 
+# 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+#It says there are no commits. It's telling me that there are no files being moved to or from the terminal to GitHub or vise versa. 
+# 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+#The most frustrating error I encountered this class was the cloning and pulling from github to my computer. I resolved it by following the directions on pulling and pushing between Git and GitHub. 
+# 9) How does cloning a repository differ from pulling from a repository?
+#Cloning a repository completely clones a file from GitHub to your terminal in your chosen location. Pulling from a repository only updates that same repository that lives in your terminal. 
+# 10) Tell me a fun fact!
+#I really enjoy prospecting for gold in the foothills of the Sierra Nevadas. 
